1d7d3a4ee9aab777350eab892b72b08e720695e5
Jeroen Scheeres <jscheeres@gmail.com>
2025-06-08 20:14:46 +0200

    Update package sources

--- setup.sh
+++ setup.sh
@@ -18,17 +18,26 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-echo "1. Backing up existing user configs..."
-mkdir -p ./backup
-cp -r /home/$USERNAME/.config/sway ./backup/
-cp -r /home/$USERNAME/.config/waybar ./backup/
-cp -r /home/$USERNAME/.config/wofi ./backup/
+ # Add package sources and keys
+
+wget -O- https://packagecloud.io/slacktechnologies/slack/gpgkey | gpg --dearmor > /usr/share/keyrings/slack.gpg
+echo "deb [signed-by=/usr/share/keyrings/slack.gpg] https://packagecloud.io/slacktechnologies/slack/debian/ jessie main" > /etc/apt/sources.list.d/slack.list
+
+wget -O- https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /usr/share/keyrings/google-chrome.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] https://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
+
+wget -O- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /usr/share/keyrings/vscode.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/code stable main' > /etc/apt/sources.list.d/vscode.list
+
+# Update system
 
 echo "1. Updating system..."
 apt update && apt upgrade -y
 
+# Install essential packages
+
 echo "2. Installing essential packages..."
-apt install -y sudo curl wget git htop unzip ca-certificates light sway swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome wireplumber
+apt install -y sudo curl wget git htop unzip ca-certificates light sway swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome wireplumber slack-desktop google-chrome-stable code
 
 echo "3. Creating directories..."
 mkdir -p ~/.config/sway ~/.config/waybar ~/.config/wofi ~/downloads
@@ -46,8 +55,3 @@ fi
 echo "6. Downloading wallpaper..."
 wget -O /home/$USERNAME/.config/sway/wallpaper.jpg https://www.learnlinux.tv/wp-content/uploads/2023/11/wallpaper.jpg
 chown $USERNAME:$USERNAME /home/$USERNAME/.config/sway/wallpaper.jpg
-
-echo "4. Installing Google Chrome..."
-wget -O /home/$USERNAME/downloads/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y /tmp/google-chrome.deb
-rm /home/$USERNAME/downloads/google-chrome.deb
