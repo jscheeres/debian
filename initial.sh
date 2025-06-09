6fb8d947f471c3b44820817b630f9d11220924ce
Jeroen Scheeres <jscheeres@gmail.com>
2025-06-09 08:18:11 +0200

    Move script

--- initial.sh
+++ initial.sh
@@ -1,11 +1,7 @@
 apt update && apt upgrade -y
 apt install -y git gpg
 
-rm -f ~/.ssh/github*
-ssh-keygen -t ed25519 -f ~/.ssh/github -N ""
 
-wget -O- https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg --dearmor > /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main' > /etc/apt/sources.list.d/github-cli.list
 
 apt update && apt install gh -y
 
--- setup.sh
+++ setup.sh
@@ -3,12 +3,13 @@
 set -e
 
 # Check arguments
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <username>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <username> <password>"
     exit 1
 fi
 
 USERNAME="$1"
+PASSWORD="$2"
 
 echo "=== Debian Automatic Setup Script ==="
 
@@ -18,7 +19,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
- # Add package sources and keys
+# Add package sources and keys
 
 wget -O- https://packagecloud.io/slacktechnologies/slack/gpgkey | gpg --dearmor > /usr/share/keyrings/slack.gpg
 echo "deb [signed-by=/usr/share/keyrings/slack.gpg] https://packagecloud.io/slacktechnologies/slack/debian/ jessie main" > /etc/apt/sources.list.d/slack.list
@@ -29,13 +30,36 @@ echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] https://d
 wget -O- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /usr/share/keyrings/vscode.gpg
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/code stable main' > /etc/apt/sources.list.d/vscode.list
 
+wget -O- https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg --dearmor > /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main' > /etc/apt/sources.list.d/github-cli.list
+
 # Update system
 
 apt update && apt upgrade -y
 
 # Install essential packages
 
-apt install -y sudo curl wget git htop unzip ca-certificates light sway swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome wireplumber slack-desktop google-chrome-stable code
+apt install -y sudo curl wget gpg git htop unzip ca-certificates light sway swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome wireplumber gh
+#slack-desktop google-chrome-stable code
+
+# Add the user
+
+adduser --disabled-password --gecos "" "$USERNAME"
+echo "$USERNAME:$PASSWORD" | chpasswd
+usermod -aG sudo "$USERNAME"
+
+# Switch to the new user
+su - "$USERNAME"
+
+# Generate SSH keys for the user
+rm -f ~/.ssh/github*
+ssh-keygen -t ed25519 -f ~/.ssh/github -N ""
+
+# Clone repository from github
+
+gh auth login
+gh ssh-key add ~/.ssh/github.pub --title "Debian CLI Key"
+git clone git@github.com:jscheeres/debian.git ~/debian
 
 # Copy sway, waybar, and wofi configuration
 
