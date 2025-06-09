e0e373be146f2ec6164716f15fd291b1c9c690e7
Jeroen Scheeres <jscheeres@gmail.com>
2025-06-09 08:08:14 +0200

    Update

--- initial.sh
+++ initial.sh
@@ -13,4 +13,4 @@ gh auth login
 
 gh ssh-key add ~/.ssh/github.pub --title "Debian CLI Key"
 
-git clone git@github.com:jscheeres/debian.git ~
+git clone git@github.com:jscheeres/debian.git ~/debian
